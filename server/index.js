2ecedf8eae517a76e1216c58106f08d42b84afdf
Michael Corey Munn <coreymunn3@gmail.com>
2022-02-23 09:41:39 -0500

    socket handlers for incoming messages and sending messages, helper functions to manage users

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const http = require('http');
 const { Server } = require('socket.io');
 const cors = require('cors');
+const { addUser, removeUser, getUser, getUserInRoom } = require('./users');
 
 const app = express();
 const httpServer = http.createServer(app);
@@ -15,8 +16,31 @@ const io = new Server(httpServer, {
 io.on('connection', (socket) => {
   console.log('we have a connection');
 
-  socket.on('join', (args, callback) => {
-    console.log(args);
+  socket.on('join', ({ name, room }, callback) => {
+    console.log(name, room);
+    const { user, error } = addUser({
+      id: socket.id,
+      name,
+      room,
+    });
+    if (error) return callback(error);
+
+    socket.emit('message', {
+      user: 'admin',
+      text: `Welcome, ${user.name} - room ${user.room}`,
+    });
+    socket.broadcast
+      .to(user.room)
+      .emit('message', { user: 'admin', text: `${user.name} has joined` });
+    socket.join(user.room);
+
+    callback();
+  });
+
+  socket.on('sendMessage', (message, callback) => {
+    const user = getUser(socket.id);
+    io.to(user.room).emit('message', { user: user.name, text: message });
+    callback();
   });
 
   socket.on('disconnect', () => {
